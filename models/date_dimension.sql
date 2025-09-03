69e564f63a12ee49a39585b76d99e4d27ce5ec60
Piyush Dhondiyal <dm8581746@gmail.com>
2025-09-03 18:04:10 +0000

    date dimension model

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,6 +1,16 @@
 with cte as (
-    select started_at
+    select to_timestamp(started_at) as started_time,
+    date(started_time) as date_started_at,
+    hour(started_time) as hour_started_at,
+    case when dayname(to_timestamp(started_at)) in ('Sat','Sun')
+    then 'Weekend' else 'Businessday' end as day_type,
+    case when month(to_timestamp(started_at)) in (12,1,2) then 'Winter'
+         when month(to_timestamp(started_at)) in (3,4,5) then 'Spring'
+         when month(to_timestamp(started_at)) in (6,7,8) then 'Summer'
+    else 'Autumn' end as station_of_year
+
     from {{ source('demo', 'bike') }}
+    where started_at != 'started_at'
 )
 
 select * from cte
