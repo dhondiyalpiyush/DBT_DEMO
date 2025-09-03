af50d6e6330c71d96ef028fb59501ffc0a6b33a3
Piyush Dhondiyal <dm8581746@gmail.com>
2025-09-03 18:58:12 +0000

    daily weather model created

---
+++ macros/date_utils.sql
@@ -0,0 +1,16 @@
+{% macro station(x)%}
+
+case when month(to_timestamp({{x}})) in (12,1,2) then 'Winter'
+     when month(to_timestamp({{x}})) in (3,4,5) then 'Spring'
+     when month(to_timestamp({{x}})) in (6,7,8) then 'Summer'
+     else 'Autumn' end
+
+{% endmacro %}
+
+
+
+{% macro dayweek(x)%}
+case when dayname(to_timestamp({{x}})) in ('Sat','Sun') then 'Weekend'
+     else 'Weekday' end
+
+{% endmacro %}
