2532fd1ef89f100e1ca490d0a5a7009648c921ae
Piyush Dhondiyal <dm8581746@gmail.com>
2025-09-03 16:37:41 +0000

    my first dbt project commit

--- models/data_dimension.sql
+++
@@ -1,6 +0,0 @@
-with cte as (
-    select started_at
-    from {{ source('DEMO', 'BIKE') }}
-)
-
-select * from cte;
---
+++ models/date_dimension.sql
@@ -0,0 +1,6 @@
+with cte as (
+    select started_at
+    from {{ source('demo', 'bike') }}
+)
+
+select * from cte
